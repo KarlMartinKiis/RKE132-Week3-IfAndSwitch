386066d2a6a3d185703bf7a4f9a4dbda83c2f775
Kiis <karkiis@tktk.ee>
2024-02-19 21:48:12 +0200

    add week3 code

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+Console.WriteLine("Enter a number");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+//kas kasutaja arv on paaris voi paaritu
+
+int result = userNum % 2;
+
+if(result != 0) //!= - ei ole 
+{
+    Console.WriteLine("user number is odd.");
+}
+else
+{
+    Console.WriteLine("User number is even.");
+}
+
